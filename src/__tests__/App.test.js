83453fb5990fa2821797414dc6dcb91e13aec4f5
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-06-20 10:17:24 -0600

    Create CitySearch test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,14 +2,23 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
+import CitySearch from '../CitySearch';
 
 describe('<App /> component', () => {
+  let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
+  });
   // test description
   test('render list of events', () => {
     // render component 
-    const AppWrapper = shallow(<App />);
     // expect function runs a search find() for EventList components within AppWrapper
     // It ensures that there exists only one EventList component within the App Component
     expect(AppWrapper.find(EventList)).toHaveLength(1);
   })
+  // test if CitySearch component is in App Component
+  test('render CitySearch', () => {
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
+
 });
