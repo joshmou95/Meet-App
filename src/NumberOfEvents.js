ba8ea4c8426a0156af9fc381afcacd8f50d5e3e9
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-06-22 10:00:55 -0600

    Added input box and state change

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
+  state = {
+    eventNumber: 32,
+  }
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.setState({ 
+      eventNumber: value,
+     });
+  }
+
   render() {
     return (
-      <div className='event-number'>
+      <div className='NumberOfEvents'>
+        <input 
+          type="number"
+          className='event-number'
+          value={this.state.eventNumber}
+          onChange={this.handleInputChanged}
+        />
         
       </div>
     );
