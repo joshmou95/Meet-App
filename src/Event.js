6d025a422acd18d13d22f11b04f2e30533c35dd1
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-06-21 21:41:00 -0600

    Add event details

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
 
+// (summary, start, htmlLink, description)
 class Event extends Component {
+  state = {
+    event: {},
+    showDetails: false
+  }
+
+  showDetails = () => {
+    this.setState({
+      showDetails: !this.state.showDetails
+    })
+  }
+
   render() {
-    return <div></div>;
+    const { events } = this.props;
+    return <div className='event'>
+      <h1 className='name'>{events.summary}</h1>
+      <p>{events.start}</p>
+
+      {this.state.showDetails && (
+        <div className='event-details'>
+          <h2>About event:</h2>
+          <a href={events.htmlLink}>See Details on Google Calendar</a>
+           <p>{events.description}</p>
+        </div>
+      )}
+
+      <button className='details-btn' onClick={this.showDetails}>{this.state.showDetails ? 'Hide Details' : 'Show Details'}</button>
+    </div>
   }
 }
 
