815995bd8eab913a1f88e56a2dfd90769f9849a3
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-06-21 22:15:29 -0600

    test for NumberOfEvents

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,22 +3,27 @@ import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
+import NumberOfEvents from '../NumberOfEvents';
+
+
 
 describe('<App /> component', () => {
   let AppWrapper;
   beforeAll(() => {
     AppWrapper = shallow(<App />);
   });
-  // test description
+
+  // test if 1 EventList component is in App Component
   test('render list of events', () => {
-    // render component 
-    // expect function runs a search find() for EventList components within AppWrapper
-    // It ensures that there exists only one EventList component within the App Component
     expect(AppWrapper.find(EventList)).toHaveLength(1);
   })
-  // test if CitySearch component is in App Component
+
+  // test if 1 CitySearch component is in App Component
   test('render CitySearch', () => {
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
-
+  
+  test('render NumberOfEvents', () => {
+    expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
+  })
 });
