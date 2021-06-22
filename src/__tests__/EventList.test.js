8e6fb7526a85b4734c54cf55ad9aa439ed5bd8a4
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-06-21 21:41:50 -0600

    Update EventList.test.js

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -7,8 +7,13 @@ import { mockData } from '../mock-data';
 describe('<EventList /> component', () => {
   // test will pass if EventList renders four events from its prop events using Mock Data
   // Events data list will be passed as props to EventList from the parent App component
+  let EventListWrapper;
+  beforeAll(() => {
+    EventListWrapper = shallow(<EventList events={mockData} />);
+  });
+
   test('render correct number of events', () => {
-    const EventListWrapper = shallow(<EventList events={mockData} />);
     expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
   });
+
 });
