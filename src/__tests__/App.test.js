d73de50a8ea3c5f2c36823fcd7bf8677ce5182f1
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-06-24 12:44:27 -0600

    built integration tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
 import NumberOfEvents from '../NumberOfEvents';
+import { mockData } from '../mock-data';
+import { extractLocations } from '../api';
 
 
 
@@ -27,3 +29,52 @@ describe('<App /> component', () => {
     expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
   })
 });
+
+// integration tests
+describe('<App /> integration', () => {
+
+  // Test if EventList gets events as a prop from App
+  test('App passes "events" state as a prop to EventList', () => {
+    // render App Component
+    const AppWrapper = mount(<App />);
+    const AppEventsState = AppWrapper.state('events');
+    // test that state is not undefined
+    expect(AppEventsState).not.toEqual(undefined);
+    // compare state to props
+    expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState);
+    // unmount after test
+    AppWrapper.unmount();
+  });
+
+  test('App passes "locations" state as a prop to CitySearch', () => {
+    const AppWrapper = mount(<App />);
+    const AppLocationsState = AppWrapper.state('locations');
+    expect(AppLocationsState).not.toEqual(undefined);
+    expect(AppWrapper.find(CitySearch).props().locations).toEqual(AppLocationsState);
+    AppWrapper.unmount();
+  });
+
+  test('get list of events matching the city selected by the user', async () => {
+    const AppWrapper = mount(<App />);
+    const CitySearchWrapper = AppWrapper.find(CitySearch);
+    const locations = extractLocations(mockData);
+    // suggestions state set to all available cities
+    CitySearchWrapper.setState({ suggestions: locations });
+    const suggestions = CitySearchWrapper.state('suggestions');
+    // hold the index of the selected suggestion from the suggestions array
+    const selectedIndex = Math.floor(Math.random() * (suggestions.length));
+    // once index is selected it is used to return the suggestion in selectedCity
+    const selectedCity = suggestions[selectedIndex];
+    // click is mimicked in CitySearchWrapper instance, selectedCity is passed to it
+    await CitySearchWrapper.instance().handleItemClicked(selectedCity);
+    // getEvents from the API ascynchronously
+    const allEvents = await getEvents();
+    // list of events is filtered against the selected city 
+    const eventsToShow = allEvents.filter(event => event.location === selectedCity);
+    // compares state of events to eventsToShow
+    expect(AppWrapper.state('events')).toEqual(eventsToShow);
+    AppWrapper.unmount();
+
+  })
+
+})
