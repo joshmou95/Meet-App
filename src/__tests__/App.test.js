9705b6bc98cd7c47b814acc38cb65ea10c58f875
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-06-26 17:37:58 -0600

    added test for all cities

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -75,4 +75,13 @@ describe('<App /> integration', () => {
     AppWrapper.unmount();
   });
 
+  // simulates click on the last list item then checks the events state to equal all events
+  test('get list of all events when use selects "See all cities"', async () => {
+    const AppWrapper = mount(<App />);
+    const suggestionItems = AppWrapper.find(CitySearch).find('.suggestions li');
+    await suggestionItems.at(suggestionItems.length -1).simulate('click');
+    const allEvents = await getEvents();
+    expect(AppWrapper.state('events')).toEqual(allEvents);
+    AppWrapper.unmount();
+  });
 })
