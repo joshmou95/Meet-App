f8af5af88b9a549cde83560db28914fdba7d17f1
Joshua Mouton <80426764+joshmou95@users.noreply.github.com>
2021-06-30 13:33:32 -0600

    removed state numberOfEvents

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
-  state = {
-    numberOfEvents: 32
-  }
+  // state = {
+  //   numberOfEvents: 32
+  // }
 
   handleChange = (event) => {
     console.log('handleChange event', event)
     const value = event.target.value;
-    this.setState({ 
-      numberOfEvents: value,
-     });
+    // this.setState({ 
+    //   numberOfEvents: value,
+    //  });
      console.log('handleChange numberOfEvents', value)
      this.props.updateEvents(value);
   }
