517a01a5c45f781b34662d4efe6f88a12d240278
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-07-03 18:08:50 -0600

    Styled CitySearch

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
 
 class CitySearch extends Component {
   state = {
@@ -25,32 +26,35 @@ class CitySearch extends Component {
     });
     // pass the clicked suggestion to the passed updateEvents prop
     this.props.updateEvents(suggestion);
-    console.log('handleItemClicked', suggestion);
+    // console.log('handleItemClicked', suggestion);
   }
 
   render() {
     return (
-      <div className="CitySearch">
-        <input 
-          type="text"
-          className="city"
-          value={this.state.query}
-          onChange={this.handleInputChanged}
-          onFocus={() => {this.setState({ showSuggestions: true }) }}
-        />
-        {/* if showSuggestions is true the list will be visible, otherwise not */}
-        <ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
-          {this.state.suggestions.map((suggestion) => (
-            <li 
-            key={suggestion}
-            onClick={() => this.handleItemClicked(suggestion)}
-            >{suggestion}</li>
-          ))}
-          <li onClick={() => this.handleItemClicked("all")}>
-            <b>See all cities</b>
-          </li>
-        </ul>
-      </div>
+      <Form>
+        <Form.Label>Type City Name or See All Cites:</Form.Label>
+        <Form.Group className="CitySearch mb-3">
+          <Form.Control 
+            type="text"
+            className="city"
+            value={this.state.query}
+            onChange={this.handleInputChanged}
+            onFocus={() => {this.setState({ showSuggestions: true }) }}
+          />
+          {/* if showSuggestions is true the list will be visible, otherwise not */}
+          <ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
+            {this.state.suggestions.map((suggestion) => (
+              <li 
+              key={suggestion}
+              onClick={() => this.handleItemClicked(suggestion)}
+              >{suggestion}</li>
+            ))}
+            <li onClick={() => this.handleItemClicked("all")}>
+              <b>See all cities</b>
+            </li>
+          </ul>
+        </Form.Group>
+      </Form>
     );
   }
 }
