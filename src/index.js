06f53fd0c48657f03d2ad559f09502d30aedc763
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-07-04 21:29:41 -0600

    added atatus

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import * as atatus from 'atatus-spa';
+
+atatus.config('5f78089c75464a44b35b4e5a82927e28').install();
 
 // entry point of the build
 ReactDOM.render(
@@ -23,3 +26,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// atatus.notify(new Error('Test Atatus Setup'));
