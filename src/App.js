bef2b0ac1b861af773f964fe0e365c1dc6055b30
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-07-11 23:33:07 -0600

    Fixed numberOfEvents state

--- src/App.js
+++ src/App.js
@@ -13,9 +13,11 @@ class App extends Component {
     this.state = {
       events: [],
       locations: [],
-      numberOfEvents: 10
+      numberOfEvents: 32,
+      currentLocation: 'all'
     }
-    this.handleChange = this.handleChange.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
+
   }
 
   componentDidMount() {
@@ -23,7 +25,9 @@ class App extends Component {
     getEvents().then((events) => {
       if (this.mounted) {
       // make API call and save initial data to state
-      this.setState({ events, locations: extractLocations(events) });
+      this.setState({ 
+        events: events.slice(0, this.state.numberOfEvents), 
+        locations: extractLocations(events) });
       }
     });
   }
@@ -32,32 +36,63 @@ class App extends Component {
     this.mounted = false;
   }
 
-  updateEvents = (location) => {
-    const eventCount = this.state.numberOfEvents;
-    // console.log('initial location', location);
-    // console.log('initial eventCount', eventCount);
-    getEvents().then((events) => {
-      // check if value is 'all'
-      // console.log('events from updateEvents', events);
-      const locationEvents = (location === 'all')
-      ? events.slice(0, eventCount)
-      :
-      events.filter((event) => event.location === location);
-      this.setState({
-        events: locationEvents.slice(0, eventCount)
+  updateEvents = (location, eventCount) => {
+    const { currentLocation, numberOfEvents } = this.state;
+    if (location) {
+      getEvents().then((events) => {
+        const locationEvents = (location === 'all') 
+        ? events 
+        : events.filter((event) => event.location === location);
+        const filteredEvents = locationEvents.slice(0, numberOfEvents);
+        this.setState({
+          events: filteredEvents,
+          currentLocation: location
+        });
       });
-      // console.log('locationEvents', locationEvents);
-      // console.log('locationEvents Count', eventCount);
-    });
+    } else {
+      getEvents().then((events) => {
+        const locationEvents = (currentLocation === 'all') 
+        ? events 
+        : events.filter((event) => event.location === currentLocation);
+        const filteredEvents = locationEvents.slice(0, eventCount);
+        this.setState({
+          events: filteredEvents,
+          numberOfEvents: eventCount
+        });
+      });
+    }
   }
 
-  handleChange(event) {
-    this.setState({
-      numberOfEvents: event.target.value
-    })
-  }
+
+  // updateEvents = (location, eventCount) => {
+  //   const { numberOfEvents } = this.state
+  //   // console.log('initial location', location);
+  //   console.log('initial state', numberOfEvents);
+  //   getEvents().then((response) => {
+  //     // check if value is 'all'
+  //     // console.log('events from updateEvents', events);
+  //     const locationEvents = (location === 'all')
+  //     ? response.events
+  //     :
+  //     response.events.filter((event) => event.location === location);
+  //     this.setState({
+  //       events: locationEvents.slice(0, numberOfEvents),
+  //       numberOfEvents: eventCount
+  //     });
+  //     // console.log('locationEvents', locationEvents);
+  //     // console.log('locationEvents Count', eventCount);
+  //   });
+  // }
+
+  // handleChange = (event) => {
+  //   // this.updateEvents();
+  //   this.setState({
+  //     numberOfEvents: event.target.value,
+  //   });
+  // }
 
   render() {
+    const { numberOfEvents } = this.state
     return (
       <Container className="App" bg="dark">
         <h1 className="mb-2">Meet App</h1>
@@ -65,8 +100,10 @@ class App extends Component {
         locations={this.state.locations} 
         updateEvents={this.updateEvents} />
         <NumberOfEvents 
-        input={this.state.numberOfEvents} 
-        handleChange={this.handleChange} />
+        // input={this.state.numberOfEvents} 
+        // handleChange={this.handleChange} 
+        numberOfEvents={numberOfEvents}
+        updateEvents={this.updateEvents} />
         <EventList 
         events={this.state.events} />
       </Container>
