c5eb4c231d3f9682d0b1bcdd935630fbab140ac3
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-07-11 23:33:37 -0600

    Update NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -2,31 +2,33 @@ import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 
 class NumberOfEvents extends Component {
-  // state = {
-  //   evenCount: 32
-  // }
+  state = {
+    numberOfEvents: 32
+  }
 
-  // handleChange = (event) => {
-  //   console.log('handleChange event', event)
-  //   const value = event.target.value;
-  //   this.setState({ 
-  //     eventCount: value,
-  //    });
-     
-  //    this.props.updateEvents(value);
-  //    console.log('handleChange numberOfEvents', value)
-  // }
+  handleChange = (event) => {
+    // console.log('handleChange event', event)
+    const value = event.target.value;
+    this.setState({ 
+      numberOfEvents: value,
+     });
+     this.props.updateEvents(null, value);
+     console.log('handleChange numberOfEvents', value)
+  }
 
 
   render() {
+    const { numberOfEvents } = this.state
     return (
       <Form.Group className='numberOfEvents mb-3'>
           <Form.Label size="sm">Number of Events:</Form.Label>
         <Form.Control size="sm"
           type="number"
           className='event-number'
-          value={this.props.input}
-          onChange={this.props.handleChange}
+          value={numberOfEvents}
+          max={32}
+          min={1}
+          onChange={this.handleChange}
         />
       </Form.Group>
 
