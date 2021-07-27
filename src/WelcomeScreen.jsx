a3610759afb6289732147a5f4a47e6394fa85a69
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-07-26 20:32:42 -0600

    updated url

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -29,7 +29,7 @@ function WelcomeScreen(props) {
           </div>
         </div>
         <a
-          href="https://joshmou95.github.io/meet-app/privacy.html"
+          href="https://joshmou95.github.io/Meet-App/privacy.html"
           rel="nofollow noopener"
         >
           Privacy policy
