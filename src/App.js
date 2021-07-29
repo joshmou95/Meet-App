63ee21a331dc743fc56d9cca74fd9155aa159e23
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-07-28 21:36:22 -0600

    import recharts

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './nprogress.css';
 import Container from 'react-bootstrap/Container';
 import { WarningAlert } from './Alert';
 import WelcomeScreen from './WelcomeScreen';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
 
 class App extends Component {
@@ -86,20 +87,44 @@ class App extends Component {
     }
   }
 
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    })
+    return data;
+  };
+
   render() {
     if (this.state.showWelcomeScreen === undefined) return <div className="App" />
-    const { numberOfEvents } = this.state
+    const { locations, numberOfEvents } = this.state
     return (
       <div>
         <Container className="App" bg="dark">
           <WarningAlert text={this.state.warningText} />
           <h1>Meet App</h1>
           <CitySearch 
-          locations={this.state.locations} 
+          locations={locations} 
           updateEvents={this.updateEvents} />
           <NumberOfEvents 
           numberOfEvents={numberOfEvents}
           updateEvents={this.updateEvents} />
+          <h4>Events in each city</h4>
+          <ScatterChart
+            width={400}
+            height={400}
+            margin={{
+              top: 20, right: 20, bottom: 20, left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis type="number" dataKey="number" name="number of events" />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
           <EventList 
           events={this.state.events} />
           <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
