c607453cc536a182adcc5c31ece68e92b479c4b8
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-07-31 10:24:56 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -90,10 +90,10 @@ class App extends Component {
 
   getData = () => {
     const { locations, events } = this.state;
-    let data = locations.map((location)=>{
+    const data = locations.map((location)=>{
       const number = events.filter((event) => event.location === location).length
       const city = location.split(', ').shift()
-      return {city, number};
+      return { city, number };
     })
     return data;
   };
@@ -119,12 +119,12 @@ class App extends Component {
               <ScatterChart width={800} height={400}
                 margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
                 <CartesianGrid />
-                <XAxis type="category" dataKey="city" name="city" />
+                <XAxis type="category" dataKey="city" name="City" />
                 <YAxis 
                   allowDecimals={false} 
                   type="number" 
                   dataKey="number" 
-                  name="number of events" />
+                  name="Number of events" />
                 <Tooltip cursor={{ strokeDasharray: '3 3' }} />
                 <Scatter data={this.getData()} fill="#8884d8" />
               </ScatterChart>
